91bf63af56814c96e6148e8aa3c58f08ba77dd3c
Bianca Rosa <me@biancarosa.com.br>
2021-03-30 17:35:06 -0300

    Solve image rotation

--- chapter-1/rotate_matrix.py
+++ chapter-1/rotate_matrix.py
@@ -2,14 +2,35 @@
 # Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes
 # write a method to rotate the image by 90 degrees. Can you do this in place?
 
-input_image = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]]
+input_image = [
+    [0,0,0,1], # 0,[0] -> [0],3 | 0,[1] -> [1],3 | 0,[2] -> [2],3 | 0,[3] -> [3],3 
+    [0,0,1,0], # 1,[0] -> [0],2 | [,[1] -> [1],2 | 1,[2] -> [2],2 | 1,[3] -> [3],2 
+    [0,1,0,0], # 2,[0] -> [0],1 | 2,[1] -> [1],1 | 2,[2] -> [2],1 | 2,[3] -> [3],1 
+    [1,0,0,0]  # 3,[0] -> [0],0 | 3,[1] -> [1],0 | 3,[2] -> [2],0 | 3,[3] -> [3],0 
+]
 
-image_rotated = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+image_rotated = [
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,1]
+]
 
 def rotate_image(img):
-    pass
+    output = []
+    rows = len(img)
+    cols = len(img[0])
 
-if rotate_image(input_image) == rotate_image:
+    for i in range(rows):
+        output.append([9] * cols)
+
+    for i in range(rows):
+        for j in range(cols):
+            output[i][j] = img[j][(cols-i-1)]
+
+    return output
+
+if rotate_image(input_image) == image_rotated:
     print("Image rotated sucessfully!")
 else:
     print("Dude... image wasn't rotated....")
