66a8405b63acb02ad13534f24e09f668f39be25b
Bianca Rosa <me@biancarosa.com.br>
2021-03-30 13:52:44 -0300

    Add skeleton for rotate matrix problem

---
+++ chapter-1/rotate_matrix.py
@@ -0,0 +1,15 @@
+# 7 - Rotate Matrix - rotate_matrix.py 
+# Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes
+# write a method to rotate the image by 90 degrees. Can you do this in place?
+
+input_image = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]]
+
+image_rotated = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+def rotate_image(img):
+    pass
+
+if rotate_image(input_image) == rotate_image:
+    print("Image rotated sucessfully!")
+else:
+    print("Dude... image wasn't rotated....")
